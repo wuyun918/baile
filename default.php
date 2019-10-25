3b2fb8a33518a4cad71d378650414d1ca9e4ad84
wuyun918 <56991461+wuyun918@users.noreply.github.com>
2019-10-25 13:43:52 +0800

    Add files via upload

---
+++ default.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+define('IN_ECS', true);
+
+require(dirname(__FILE__) . '/includes/init.php');
+
+if ((DEBUG_MODE & 2) != 2)
+{
+    $smarty->caching = true;
+}
+$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+$uachar = "/(nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|mobile)/i";
+
+if(($ua == '' || preg_match($uachar, $ua))&& !strpos(strtolower($_SERVER['REQUEST_URI']),'wap'))
+{
+    $Loaction = 'index.php';
+
+    if (!empty($Loaction))
+    {
+        ecs_header("Location: $Loaction\n");
+
+        exit;
+    }
+
+}
+else
+{
+    $Loaction = 'pretty/';
+
+    if (!empty($Loaction))
+    {
+        ecs_header("Location: $Loaction\n");
+
+        exit;
+    }
+}
+
+?>
